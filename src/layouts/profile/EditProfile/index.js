560aaf2698d83a252dd40b50850e1ceb091a46fe
Prajwal kakkar <prajwal@ixiono.com>
2022-02-28 19:48:24 +0530

    added admin update

--- src/layouts/profile/EditProfile/index.js
+++ src/layouts/profile/EditProfile/index.js
@@ -11,7 +11,7 @@ import { toast } from "react-toastify";
 
 // Authentication layout components
 import CoverLayout from "layouts/authentication/components/CoverLayout";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 // import { useDispatch, useSelector } from "react-redux";
 // import { updateProfile } from "../../../actions/userActions";
@@ -23,100 +23,127 @@ import axios from "axios";
 import bgImage from "assets/images/bg-sign-up-cover.jpeg";
 
 function Cover() {
-	const [values, setValues] = useState({
-		name: "",
-		email: "",
-		password: "",
-	});
-	const { name, email, password } = values;
-	const handleChange = (event) => {
-		setValues({
-			...{ name, email, password },
-			[event.target.name]: event.target.value,
-		});
-	};
-	// const [name, setName] = useState("");
-	// const [email, setEmail] = useState("");
-	// const [password, setPassword] = useState("");
-	// const updateProfile = { email, password };
-	// const navigate = useNavigate();
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [oldPass, setOldPass] = useState();
+  const { name, email, password } = values;
+  useEffect(() => {
+    setOldPass(values.password);
+    axios
+      .get(
+        "https://danerob-api.herokuapp.com/admin/get-profile?email=admin%40mail.com"
+      )
+      .then((res) => {
+        setValues({
+          name: res.data.name,
+          email: res.data.email,
+          password: "",
+        });
+      });
+  }, []);
+  const handleChange = (event) => {
+    setValues({
+      ...{ name, email, password },
+      [event.target.name]: event.target.value,
+    });
+  };
+  // const [name, setName] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  // const updateProfile = { email, password };
+  // const navigate = useNavigate();
 
-	// axios.get("https://danerob-api.herokuapp.com/admin/get-one?email=testadmin%40mail.com")
+  // axios.get("https://danerob-api.herokuapp.com/admin/get-one?email=testadmin%40mail.com")
 
-	function onSubmit(event) {
-		event.preventDefault();
-		console.log(values);
-		const payload = {
-			newName: values.name,
-			newEmail: values.email,
-			newPassword: values.password,
-		};
+  function onSubmit(event) {
+    event.preventDefault();
+    let payload;
+    console.log("old password", oldPass);
+    console.log("new password", values.password);
+    if (values.password !== oldPass) {
+      console.log("password is changed", values.password);
+      payload = {
+        newName: values.name,
+        newEmail: values.email,
+        newPassword: values.password,
+      };
+    } else {
+      console.log("password did not change");
+      payload = {
+        newName: values.name,
+        newEmail: values.email,
+      };
+    }
 
-		axios
-			.patch(
-				"https://danerob-api.herokuapp.com/admin/update-admin?email=testadmin%40mail.com",
-				payload
-			)
-			.then((res) => {
-				if (res.status === 200) {
-					toast.success("Details changed successfully");
-				}
-				console.log(res.data);
-			})
-			.catch((error) => console.log("error", error));
-	}
+    axios
+      .patch(
+        "https://danerob-api.herokuapp.com/admin/update-admin?email=admin%40mail.com",
+        payload
+      )
+      .then((res) => {
+        if (res.status === 200) {
+          toast.success("Details changed successfully");
+        }
+        console.log(res.data);
+      })
+      .catch((error) => console.log("error", error));
+  }
 
-	// const [email, setEmail] = useState("");
-	// const [password, setPassword] = useState("");
-	// const [confirmPassword, setConfirmPassword] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [confirmPassword, setConfirmPassword] = useState("");
 
-	// const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-	// const userLogin = useSelector((state) => state.userLogin);
-	// const { userInfo } = userLogin;
+  // const userLogin = useSelector((state) => state.userLogin);
+  // const { userInfo } = userLogin;
 
-	// const userUpdate = useSelector((state) => state.userUpdate);
-	// const { loading, error, success } = userUpdate;
+  // const userUpdate = useSelector((state) => state.userUpdate);
+  // const { loading, error, success } = userUpdate;
 
-	// useEffect(() => {
-	// 	if (!userInfo) {
-	// 		// history.push("/");
-	// 		<Navigate to="/" />;
-	// 	} else {
-	// 		setName(userInfo.name);
-	// 		setEmail(userInfo.email);
-	// 	}
-	// }, [history, userInfo]);
+  // useEffect(() => {
+  // 	if (!userInfo) {
+  // 		// history.push("/");
+  // 		<Navigate to="/" />;
+  // 	} else {
+  // 		setName(userInfo.name);
+  // 		setEmail(userInfo.email);
+  // 	}
+  // }, [history, userInfo]);
 
-	// const submitHandler = (e) => {
-	// 	e.preventDefault();onSubmit
+  // const submitHandler = (e) => {
+  // 	e.preventDefault();onSubmit
 
-	// 	dispatch(updateProfile({ name, email, password }));
-	// };
+  // 	dispatch(updateProfile({ name, email, password }));
+  // };
 
-	return (
-		<CoverLayout image={bgImage}>
-			<Card>
-				<MDBox
-					variant="gradient"
-					bgColor="info"
-					borderRadius="lg"
-					coloredShadow="success"
-					mx={2}
-					mt={-3}
-					p={3}
-					mb={1}
-					textAlign="center">
-					<MDTypography variant="h4" fontWeight="medium" color="white" mt={1}>
-						Edit Your Profile
-					</MDTypography>
-					<MDTypography display="block" variant="button" color="white" my={1}>
-						Enter new datails
-					</MDTypography>
-				</MDBox>
-				<MDBox pt={4} pb={3} px={3}>
-					<MDBox component="form" role="form">
-						{/* {loading && <Loading />}
+  return (
+    <CoverLayout image={bgImage}>
+      <Card>
+        <MDBox
+          variant="gradient"
+          bgColor="info"
+          borderRadius="lg"
+          coloredShadow="success"
+          mx={2}
+          mt={-3}
+          p={3}
+          mb={1}
+          textAlign="center"
+        >
+          <MDTypography variant="h4" fontWeight="medium" color="white" mt={1}>
+            Edit Your Profile
+          </MDTypography>
+          <MDTypography display="block" variant="button" color="white" my={1}>
+            Enter new datails
+          </MDTypography>
+        </MDBox>
+        <MDBox pt={4} pb={3} px={3}>
+          <MDBox component="form" role="form">
+            {/* {loading && <Loading />}
 						{success && (
 							<ErrorMessage variant="success">
 								Updated Successfully
@@ -124,41 +151,41 @@ function Cover() {
 						)}
 						{error && <ErrorMessage variant="danger">{error}</ErrorMessage>} */}
 
-						<MDBox mb={2}>
-							<MDInput
-								type="text"
-								name="name"
-								onChange={(e) => handleChange(e)}
-								value={name}
-								label="Name"
-								variant="standard"
-								fullWidth
-							/>
-						</MDBox>
-						<MDBox mb={2}>
-							<MDInput
-								type="email"
-								name="email"
-								label="Email"
-								onChange={(e) => handleChange(e)}
-								value={email}
-								variant="standard"
-								fullWidth
-							/>
-						</MDBox>
-						<MDBox mb={2}>
-							<MDInput
-								name="password"
-								type="password"
-								// disabled={true}
-								label="Password"
-								onChange={(e) => handleChange(e)}
-								value={password}
-								variant="standard"
-								fullWidth
-							/>
-						</MDBox>
-						{/* <MDBox mb={2}>
+            <MDBox mb={2}>
+              <MDInput
+                type="text"
+                name="name"
+                onChange={(e) => handleChange(e)}
+                value={name}
+                label="Name"
+                variant="standard"
+                fullWidth
+              />
+            </MDBox>
+            <MDBox mb={2}>
+              <MDInput
+                type="email"
+                name="email"
+                label="Email"
+                onChange={(e) => handleChange(e)}
+                value={email}
+                variant="standard"
+                fullWidth
+              />
+            </MDBox>
+            <MDBox mb={2}>
+              <MDInput
+                name="password"
+                type="password"
+                // disabled={true}
+                label="Password"
+                onChange={(e) => handleChange(e)}
+                value={password}
+                variant="standard"
+                fullWidth
+              />
+            </MDBox>
+            {/* <MDBox mb={2}>
 							<MDInput
 								type="password"
 								label="Confirm Password"
@@ -168,20 +195,21 @@ function Cover() {
 								fullWidth
 							/>
 						</MDBox> */}
-						<MDBox mt={4} mb={1}>
-							<MDButton
-								onClick={(event) => onSubmit(event)}
-								variant="gradient"
-								color="info"
-								fullWidth>
-								Set Changes
-							</MDButton>
-						</MDBox>
-					</MDBox>
-				</MDBox>
-			</Card>
-		</CoverLayout>
-	);
+            <MDBox mt={4} mb={1}>
+              <MDButton
+                onClick={(event) => onSubmit(event)}
+                variant="gradient"
+                color="info"
+                fullWidth
+              >
+                Set Changes
+              </MDButton>
+            </MDBox>
+          </MDBox>
+        </MDBox>
+      </Card>
+    </CoverLayout>
+  );
 }
 
 export default Cover;
--- src/layouts/profile/index.js
+++ src/layouts/profile/index.js
@@ -16,7 +16,7 @@ import ProfileInfoCard from "examples/Cards/InfoCards/ProfileInfoCard";
 // Overview page components
 import Header from "layouts/profile/components/Header";
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 // import { useState } from "react";
 // import PlatformSettings from "layouts/profile/components/PlatformSettings";
 
@@ -24,56 +24,60 @@ import { useState } from "react";
 // import profilesListData from "layouts/profile/data/profilesListData";
 
 function Overview() {
-	const [name, setName] = useState("");
-	const [email, setEmail] = useState("");
-	// const [password, setPassword] = useState("");
-	// const [confirmPassword, setConfirmPassword] = useState("");
-	axios
-		.get(
-			"https://danerob-api.herokuapp.com/admin/get-profile?email=testadmin%40mail.com"
-		)
-		.then((res) => {
-			console.log(res.data.name);
-			setName(res.data.name);
-			setEmail(res.data.email);
-		});
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [confirmPassword, setConfirmPassword] = useState("");
 
-	return (
-		<DashboardLayout>
-			<DashboardNavbar />
-			<MDBox mb={2} />
-			<Header>
-				<Grid item>
-					<MDBox height="100%" mt={0.5} lineHeight={1}>
-						<MDTypography variant="h5" fontWeight="medium">
-							UPDATE YOUR PROFILE
-						</MDTypography>
-					</MDBox>
-				</Grid>
-				<MDBox mt={5} mb={3}>
-					<Grid container spacing={1}>
-						<Grid item xs={12} md={6} xl={4} sx={{ display: "flex" }}>
-							<Divider orientation="vertical" sx={{ ml: -2, mr: 1 }} />
-							<ProfileInfoCard
-								title="profile information"
-								info={{
-									fullName: name,
-									email: email,
-								}}
-								action={{ route: "", tooltip: "Edit Profile" }}
-								shadow={false}
-							/>
-							<Divider orientation="vertical" sx={{ mx: 0 }} />
-						</Grid>
-						{/* <Grid item xs={12} xl={4}>
+  useEffect(() => {
+    console.log("from use effect");
+    axios
+      .get(
+        "https://danerob-api.herokuapp.com/admin/get-profile?email=admin%40mail.com"
+      )
+      .then((res) => {
+        console.log("hey");
+        console.log(res.data.name, res.data.email);
+        setName(res.data.name);
+        setEmail(res.data.email);
+      });
+  }, []);
+  return (
+    <DashboardLayout>
+      <DashboardNavbar />
+      <MDBox mb={2} />
+      <Header>
+        <Grid item>
+          <MDBox height="100%" mt={0.5} lineHeight={1}>
+            <MDTypography variant="h5" fontWeight="medium">
+              UPDATE YOUR PROFILE
+            </MDTypography>
+          </MDBox>
+        </Grid>
+        <MDBox mt={5} mb={3}>
+          <Grid container spacing={1}>
+            <Grid item xs={12} md={6} xl={4} sx={{ display: "flex" }}>
+              <Divider orientation="vertical" sx={{ ml: -2, mr: 1 }} />
+              <ProfileInfoCard
+                title="profile information"
+                info={{
+                  fullName: name,
+                  email: email,
+                }}
+                action={{ route: "", tooltip: "Edit Profile" }}
+                shadow={false}
+              />
+              <Divider orientation="vertical" sx={{ mx: 0 }} />
+            </Grid>
+            {/* <Grid item xs={12} xl={4}>
               <ProfilesList title="conversations" profiles={profilesListData} shadow={false} />
             </Grid> */}
-					</Grid>
-				</MDBox>
-			</Header>
-			<Footer />
-		</DashboardLayout>
-	);
+          </Grid>
+        </MDBox>
+      </Header>
+      <Footer />
+    </DashboardLayout>
+  );
 }
 
 export default Overview;
